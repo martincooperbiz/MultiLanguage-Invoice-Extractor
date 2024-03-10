de9a18f6ef94da8ef4d5b0385508e56811c7e448
martincooperbiz <141577484+martincooperbiz@users.noreply.github.com>
2024-03-10 02:49:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 from dotenv import load_dotenv
 from PIL import Image
-
 import streamlit as st
 import os
-
 import google.generativeai as genai
 
-## load all the environment variables from .env
+# Load environment variables from .env
 load_dotenv()
 
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+# Input field for API key
+api_key = st.text_input("Enter Google API Key:", key="api_key")
 
-## Load Gemini pro vision model
-model=genai.GenerativeModel('gemini-pro-vision')
+# Configure Google API key
+if api_key:
+    genai.configure(api_key=api_key)
 
-def get_gemini_response(input,image,user_prompt):
-    response=model.generate_content([input,image[0],user_prompt])
+# Load Gemini pro vision model
+model = genai.GenerativeModel('gemini-pro-vision')
+
+def get_gemini_response(input, image, user_prompt):
+    response = model.generate_content([input, image[0], user_prompt])
     return response.text
 
 def input_image_details(uploaded_file):
@@ -33,30 +36,27 @@ def input_image_details(uploaded_file):
     else:
         raise FileNotFoundError("No file uploaded")
 
-
-##initialize our streamlit app
-
+# Initialize Streamlit app
 st.set_page_config(page_title="MultiLanguage Invoice Extractor")
 
 st.header("MultiLanguage Invoice Extractor")
-input=st.text_input("Input Prompt: ",key="input")
+input_text = st.text_input("Input Prompt:", key="input")
 uploaded_file = st.file_uploader("Choose an image of the invoice...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    image=Image.open(uploaded_file)
+    image = Image.open(uploaded_file)
     st.image(image, caption="Uploaded Image.", use_column_width=True)
 
-submit=st.button("Tell me about the invoice")
+submit_button = st.button("Tell me about the invoice")
 
-input_prompt="""
-You are an expert in understanding invoices. We will upload a a image as invoice
+input_prompt = """
+You are an expert in understanding invoices. We will upload an image as an invoice
 and you will have to answer any questions based on the uploaded invoice image
 """
 
-## if submit button is clicked
-
-if submit:
-    image_data=input_image_details(uploaded_file)
-    response=get_gemini_response(input_prompt,image_data,input)
-    st.subheader("The Rresponse is")
+# If submit button is clicked
+if submit_button:
+    image_data = input_image_details(uploaded_file)
+    response = get_gemini_response(input_prompt, image_data, input_text)
+    st.subheader("The Response is")
     st.write(response)
